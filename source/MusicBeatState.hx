8f1a9369a306b7fd97b165dc7df7f138ba6951c1
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-11-25 11:53:33 +0800

    Add files via upload

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -1,154 +1,183 @@
 package;
 
+#if windows
+import Discord.DiscordClient;
+#end
+import flixel.util.FlxColor;
+import openfl.Lib;
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.addons.ui.FlxUIState;
-import flixel.math.FlxRect;
-import flixel.util.FlxTimer;
-import flixel.addons.transition.FlxTransitionableState;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
-import flixel.FlxSprite;
-import flixel.util.FlxColor;
-import flixel.util.FlxGradient;
-import flixel.FlxState;
-import flixel.FlxCamera;
-import flixel.FlxBasic;
 
 class MusicBeatState extends FlxUIState
 {
-	private var curSection:Int = 0;
-	private var stepsToDo:Int = 0;
+	private var lastBeat:Float = 0;
+	private var lastStep:Float = 0;
 
 	private var curStep:Int = 0;
 	private var curBeat:Int = 0;
-
-	private var curDecStep:Float = 0;
-	private var curDecBeat:Float = 0;
+	private var curDecimalBeat:Float = 0;
 	private var controls(get, never):Controls;
-
-	public static var camBeat:FlxCamera;
 
 	inline function get_controls():Controls
 		return PlayerSettings.player1.controls;
 
-	override function create() {
-		camBeat = FlxG.camera;
-		var skip:Bool = FlxTransitionableState.skipNextTransOut;
-		super.create();
+	override function create()
+	{
+		TimingStruct.clearTimings();
+		(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
 
-		if(!skip) {
-			openSubState(new CustomFadeTransition(0.7, true));
-		}
-		FlxTransitionableState.skipNextTransOut = false;
+		if (transIn != null)
+			trace('reg ' + transIn.region);
+
+		super.create();
 	}
+
+
+	var array:Array<FlxColor> = [
+		FlxColor.fromRGB(148, 0, 211),
+		FlxColor.fromRGB(75, 0, 130),
+		FlxColor.fromRGB(0, 0, 255),
+		FlxColor.fromRGB(0, 255, 0),
+		FlxColor.fromRGB(255, 255, 0),
+		FlxColor.fromRGB(255, 127, 0),
+		FlxColor.fromRGB(255, 0 , 0)
+	];
+
+	var skippedFrames = 0;
 
 	override function update(elapsed:Float)
 	{
 		//everyStep();
-		var oldStep:Int = curStep;
+		/*var nextStep:Int = updateCurStep();
 
-		updateCurStep();
-		updateBeat();
-
-		if (oldStep != curStep)
+		if (nextStep >= 0)
 		{
-			if(curStep > 0)
-				stepHit();
-
-			if(PlayState.SONG != null)
+			if (nextStep > curStep)
 			{
-				if (oldStep < curStep)
-					updateSection();
-				else
-					rollbackSection();
+				for (i in curStep...nextStep)
+				{
+					curStep++;
+					updateBeat();
+					stepHit();
+				}
+			}
+			else if (nextStep < curStep)
+			{
+				//Song reset?
+				curStep = nextStep;
+				updateBeat();
+				stepHit();
+			}
+		}*/
+
+		if (Conductor.songPosition < 0)
+			curDecimalBeat = 0;
+		else
+		{
+			if (TimingStruct.AllTimings.length > 1)
+			{
+				var data = TimingStruct.getTimingAtTimestamp(Conductor.songPosition);
+
+				FlxG.watch.addQuick("Current Conductor Timing Seg", data.bpm);
+
+				Conductor.crochet = ((60 / data.bpm) * 1000);
+
+				var step = ((60 / data.bpm) * 1000) / 4;
+				var startInMS = (data.startTime * 1000);
+
+
+				var percent = (Conductor.songPosition - startInMS) / (data.length * 1000);
+
+				curDecimalBeat = data.startBeat + (((Conductor.songPosition/1000) - data.startTime) * (data.bpm / 60));
+				var ste:Int = Math.floor(data.startStep + ((Conductor.songPosition - startInMS) / step));
+				if (ste >= 0)
+				{
+					if (ste > curStep)
+					{
+						for (i in curStep...ste)
+						{
+							curStep++;
+							updateBeat();
+							stepHit();
+						}
+					}
+					else if (ste < curStep)
+					{
+						//Song reset?
+						curStep = ste;
+						updateBeat();
+						stepHit();
+					}
+				}
+			}
+			else
+			{
+				curDecimalBeat = (Conductor.songPosition / 1000) * (Conductor.bpm/60);
+				var nextStep:Int = Math.floor(Conductor.songPosition / Conductor.stepCrochet);
+				if (nextStep >= 0)
+				{
+					if (nextStep > curStep)
+					{
+						for (i in curStep...nextStep)
+						{
+							curStep++;
+							updateBeat();
+							stepHit();
+						}
+					}
+					else if (nextStep < curStep)
+					{
+						//Song reset?
+						curStep = nextStep;
+						updateBeat();
+						stepHit();
+					}
+				}
+				Conductor.crochet = ((60 / Conductor.bpm) * 1000);
 			}
 		}
 
-		if(FlxG.save.data != null) FlxG.save.data.fullscreen = FlxG.fullscreen;
+
+		if (FlxG.save.data.fpsRain && skippedFrames >= 6)
+			{
+				if (currentColor >= array.length)
+					currentColor = 0;
+				(cast (Lib.current.getChildAt(0), Main)).changeFPSColor(array[currentColor]);
+				currentColor++;
+				skippedFrames = 0;
+			}
+			else
+				skippedFrames++;
+
+		if ((cast (Lib.current.getChildAt(0), Main)).getFPSCap != FlxG.save.data.fpsCap && FlxG.save.data.fpsCap <= 290)
+			(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
 
 		super.update(elapsed);
 	}
 
-	private function updateSection():Void
-	{
-		if(stepsToDo < 1) stepsToDo = Math.round(getBeatsOnSection() * 4);
-		while(curStep >= stepsToDo)
-		{
-			curSection++;
-			var beats:Float = getBeatsOnSection();
-			stepsToDo += Math.round(beats * 4);
-			sectionHit();
-		}
-	}
-
-	private function rollbackSection():Void
-	{
-		if(curStep < 0) return;
-
-		var lastSection:Int = curSection;
-		curSection = 0;
-		stepsToDo = 0;
-		for (i in 0...PlayState.SONG.notes.length)
-		{
-			if (PlayState.SONG.notes[i] != null)
-			{
-				stepsToDo += Math.round(getBeatsOnSection() * 4);
-				if(stepsToDo > curStep) break;
-				
-				curSection++;
-			}
-		}
-
-		if(curSection > lastSection) sectionHit();
-	}
-
 	private function updateBeat():Void
 	{
+		lastBeat = curBeat;
 		curBeat = Math.floor(curStep / 4);
-		curDecBeat = curDecStep/4;
 	}
 
-	private function updateCurStep():Void
+	public static var currentColor = 0;
+
+	private function updateCurStep():Int
 	{
-		var lastChange = Conductor.getBPMFromSeconds(Conductor.songPosition);
-
-		var shit = ((Conductor.songPosition - ClientPrefs.noteOffset) - lastChange.songTime) / lastChange.stepCrochet;
-		curDecStep = lastChange.stepTime + shit;
-		curStep = lastChange.stepTime + Math.floor(shit);
-	}
-
-	public static function switchState(nextState:FlxState) {
-		// Custom made Trans in
-		var curState:Dynamic = FlxG.state;
-		var leState:MusicBeatState = curState;
-		if(!FlxTransitionableState.skipNextTransIn) {
-			leState.openSubState(new CustomFadeTransition(0.6, false));
-			if(nextState == FlxG.state) {
-				CustomFadeTransition.finishCallback = function() {
-					FlxG.resetState();
-				};
-				//trace('resetted');
-			} else {
-				CustomFadeTransition.finishCallback = function() {
-					FlxG.switchState(nextState);
-				};
-				//trace('changed state');
-			}
-			return;
+		var lastChange:BPMChangeEvent = {
+			stepTime: 0,
+			songTime: 0,
+			bpm: 0
 		}
-		FlxTransitionableState.skipNextTransIn = false;
-		FlxG.switchState(nextState);
-	}
+		for (i in 0...Conductor.bpmChangeMap.length)
+		{
+			if (Conductor.songPosition >= Conductor.bpmChangeMap[i].songTime)
+				lastChange = Conductor.bpmChangeMap[i];
+		}
 
-	public static function resetState() {
-		MusicBeatState.switchState(FlxG.state);
-	}
-
-	public static function getState():MusicBeatState {
-		var curState:Dynamic = FlxG.state;
-		var leState:MusicBeatState = curState;
-		return leState;
+		return lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
 	}
 
 	public function stepHit():Void
@@ -159,18 +188,15 @@ class MusicBeatState extends FlxUIState
 
 	public function beatHit():Void
 	{
-		//trace('Beat: ' + curBeat);
+		//do literally nothing dumbass
 	}
-
-	public function sectionHit():Void
+	
+	public function fancyOpenURL(schmancy:String)
 	{
-		//trace('Section: ' + curSection + ', Beat: ' + curBeat + ', Step: ' + curStep);
-	}
-
-	function getBeatsOnSection()
-	{
-		var val:Null<Float> = 4;
-		if(PlayState.SONG != null && PlayState.SONG.notes[curSection] != null) val = PlayState.SONG.notes[curSection].sectionBeats;
-		return val == null ? 4 : val;
+		#if linux
+		Sys.command('/usr/bin/xdg-open', [schmancy, "&"]);
+		#else
+		FlxG.openURL(schmancy);
+		#end
 	}
 }
